34533177ade25c920a7569a2cec0316aa30c45b2
Jason Ruesch <jruesch@degreed.com>
2021-06-21 17:34:34 -0400

    Import fork:  jasonruesch/last-successful-commit-action
    # This is the 1st commit message:
    
    chore: add debug
    
    # This is the commit message #2:
    
    ...
    
    # This is the commit message #3:
    
    ...
    
    # This is the commit message #4:
    
    v1.0.1
    
    # This is the commit message #5:
    
    chore: v1.0.2
    
    # This is the commit message #6:
    
    feat: use branch if no successful runs exist

--- dist/index.js
+++ dist/index.js
@@ -15,20 +15,23 @@ try {
   }
   const octokit = github.getOctokit(token);
   const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
+  const branch = core.getInput("branch");
   octokit.actions
     .listWorkflowRuns({
       owner,
       repo,
       workflow_id: core.getInput("workflow_id"),
       status: "success",
-      branch: core.getInput("branch"),
+      branch,
       event: "push",
     })
     .then((res) => {
+      core.debug('workflow_runs.length: ' + res.data.workflow_runs.length);
       const lastSuccessCommitHash =
         res.data.workflow_runs.length > 0
           ? res.data.workflow_runs[0].head_commit.id
-          : "";
+          : branch;
+      core.debug('lastSuccessCommitHash: ' + lastSuccessCommitHash);
       core.setOutput("commit_hash", lastSuccessCommitHash);
     })
     .catch((e) => {
