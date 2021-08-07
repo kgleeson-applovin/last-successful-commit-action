384064fdc4c88d5df507560f9404b5a0ef17cedf
dpet23 <dpet23@users.noreply.github.com>
2021-08-07 17:30:20 +1000

    Updated formatting

--- index.js
+++ index.js
@@ -1,37 +1,42 @@
-const core = require("@actions/core");
-const github = require("@actions/github");
+const core = require('@actions/core');
+const github = require('@actions/github');
 
 try {
-  const token = process.env.GITHUB_TOKEN || process.env.GH_TOKEN || core.getInput("github_token");
+  // Read GitHub Token.
+  const token = process.env.GITHUB_TOKEN || core.getInput('github_token');
   if (!token) {
-    throw Error('You must provide a GitHub Token via the action configuration parameter `github_token` or one of the following environment variables: `GITHUB_TOKEN`, `GH_TOKEN`.')
+    throw Error(
+      'You must provide a GitHub Token via the action configuration parameter `github_token`'
+      + ' or the environment variable `GITHUB_TOKEN`.'
+    )
   }
+
   const octokit = github.getOctokit(token);
-  const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
-  const branch = core.getInput("branch");
+
+  const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/');
+  const branch = core.getInput('branch');
   const params = {
     owner,
     repo,
-    workflow_id: core.getInput("workflow_id"),
-    status: "success",
+    workflow_id: core.getInput('workflow_id'),
+    status: 'success',
     branch,
   }
-  // optionally filter workflow runs by the event that triggered them
-  // unlike v1, there is no longer a default event of "push", the default event type is "any"
-  const event = core.getInput("workflow_event")
+  const event = core.getInput('workflow_event')
   if (event) {
-      params.event = event
+    params.event = event  // default event type is 'any'
   }
+
   octokit.actions
     .listWorkflowRuns(params)
     .then((res) => {
-      core.debug('workflow_runs.length: ' + res.data.workflow_runs.length);
+      core.debug(`workflow_runs.length: ${res.data.workflow_runs.length}`);
       const lastSuccessCommitHash =
         res.data.workflow_runs.length > 0
           ? res.data.workflow_runs[0].head_commit.id
           : branch;
-      core.debug('lastSuccessCommitHash: ' + lastSuccessCommitHash);
-      core.setOutput("commit_hash", lastSuccessCommitHash);
+      core.debug(`lastSuccessCommitHash: ${lastSuccessCommitHash}`);
+      core.setOutput('commit_hash', lastSuccessCommitHash);
     })
     .catch((e) => {
       core.setFailed(e.message);
