bdec0c26d660b4769ef31b49c16cba6632c99ea7
dpet23 <dpet23@users.noreply.github.com>
2021-08-07 17:31:17 +1000

    Performing manual workflow run filtering, and added debug logging
    The GitHub API doesn't always return recent runs when filtering by `status=success`. Instead, retrieve all runs from the API and manually find the most recent successful run.

--- index.js
+++ index.js
@@ -19,23 +19,29 @@ try {
     owner,
     repo,
     workflow_id: core.getInput('workflow_id'),
-    status: 'success',
+    // status: 'success',
     branch,
   }
   const event = core.getInput('workflow_event')
   if (event) {
     params.event = event  // default event type is 'any'
   }
+  core.debug(`params: ${JSON.stringify(params, null, 4)}`);
 
+  let lastSuccessCommitHash = branch
   octokit.actions
     .listWorkflowRuns(params)
     .then((res) => {
-      core.debug(`workflow_runs.length: ${res.data.workflow_runs.length}`);
-      const lastSuccessCommitHash =
-        res.data.workflow_runs.length > 0
-          ? res.data.workflow_runs[0].head_commit.id
-          : branch;
-      core.debug(`lastSuccessCommitHash: ${lastSuccessCommitHash}`);
+      core.debug(`Found ${res.data.workflow_runs.length} total workflow runs.`);
+
+      const successfulRuns = res.data.workflow_runs.filter(run => {
+        return run.status == 'completed' && run.conclusion == 'success';
+      })
+      if (successfulRuns.length > 0) {
+        lastSuccessCommitHash = successfulRuns[0].head_commit.id;
+      } else {
+        core.warning('Didn\'t find any successful runs. Using the branch name.');
+      }
       core.setOutput('commit_hash', lastSuccessCommitHash);
     })
     .catch((e) => {
